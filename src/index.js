3f04c80cc0d487843a592adcafd7737a3991645a
Rohit tiwari <rt30082004@gmail.com>
2024-12-14 01:24:26 +0530

    express Installation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from "./db/index.js";
-
-
-
+import { app } from './app.js';
 connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`app is running on port : ${process.env.PORT}`)
+    })
+}).catch((err) => {
+    console.log("Mongo DB connection failed !!", err)
+})
